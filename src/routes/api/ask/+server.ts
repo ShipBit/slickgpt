dc2bd177abed61c01f44badee946f2fd5c69cac9
th8m0z <thomas.mz8@icloud.com>
2024-01-08 13:42:51 +0100

    moderation implemented

--- src/routes/api/ask/+server.ts
+++ src/routes/api/ask/+server.ts
@@ -3,6 +3,8 @@ import type {
 	ChatCompletionMessageParam,
 	ChatCompletionCreateParamsStreaming
 } from 'openai/resources/chat';
+
+import type { Moderation } from 'openai/resources/moderations'
 import type { RequestHandler } from './$types';
 import type { OpenAiSettings } from '$misc/openai';
 import { error } from '@sveltejs/kit';
@@ -26,6 +28,40 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
 
 		const openAiKey: string = requestData.openAiKey;
 		throwIfUnset('OpenAI API key', openAiKey);
+		
+		// Handle moderation
+		const moderationUrl = 'https://api.openai.com/v1/moderations';
+
+		const textMessages = messages.map(msg => msg.content);
+
+		const moderationResponse = await fetch(moderationUrl, {
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Authorization': `Bearer ${openAiKey}`
+			},
+			body: JSON.stringify({ input: textMessages })
+		  });
+
+
+		if (!moderationResponse.ok) {
+			const err = await moderationResponse.json();
+			throw new Error(err.error);
+		}
+
+		const moderationJson = await moderationResponse.json()
+
+		moderationJson.results.forEach((result: Moderation, index: number) => {
+			if (result.flagged) {
+				throw new Error(`Message ${index + 1} is globally flagged for moderation.`);
+			}
+		
+			for (const [category, flagged] of Object.entries(result.categories)) {
+				if (flagged) {
+					throw new Error(`Message ${index + 1} is flagged for ${category}.`);
+				}
+			}
+		});
 
 		const completionOpts: ChatCompletionCreateParamsStreaming = {
 			...settings,
@@ -41,7 +77,7 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
 		// 		? 'https://api.openai.com/v1/chat/completions'
 		// 		: 'https://api.openai.com/v1/completions';
 		const apiUrl = 'https://api.openai.com/v1/chat/completions';
-
+		
 		const response = await fetch(apiUrl, {
 			headers: {
 				Authorization: `Bearer ${openAiKey}`,

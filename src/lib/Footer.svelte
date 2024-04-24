d533ff47f8e90aa6cca4101ff731a0d0da5681f5
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2024-04-24 22:57:18 +0200

    add legal

--- src/lib/Footer.svelte
+++
@@ -1,9 +0,0 @@
-<div class="flex gap-2 justify-between items-center">
-	<!-- Icons -->
-	<div class="flex justify-self-center md:justify-self-start space-x-4"></div>
-
-	<div class="flex gap-2">
-		<a href="/legal">Legal</a>
-		<span>|</span>
-	</div>
-</div>
